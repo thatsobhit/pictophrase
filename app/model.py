68da25cdf2a2bc7bafa718ec8e322d2b0f6eaa97
Sobhit Singh <124906982+thatsobhit@users.noreply.github.com>
2025-04-06 20:07:55 +0530

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,8 +1,19 @@
-from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, Add
 
-def build_feature_extractor():
-    base_model = InceptionV3(weights='imagenet')
-    model = Model(inputs=base_model.input, outputs=base_model.get_layer('avg_pool').output)
+def build_caption_model(vocab_size, max_length):
+    inputs1 = Input(shape=(2048,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+
+    decoder1 = Add()([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
     return model
-#feature-extractor
